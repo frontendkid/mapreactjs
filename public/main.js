9dcf5c95a1cc57af4776a64a330c0063761370cd
frontendkid <huydong.frontend@gmail.com>
2016-11-12 16:24:07 +0700

    feat(New Project): push code to master

---
+++ public/main.js
@@ -0,0 +1,14 @@
+
+var Map = React.createClass({
+  render: function(){
+    return(
+      <h1 className="mauvang"> Huy Dong React</h1>
+    );
+  }
+});
+
+ReactDOM.render(
+  <div>
+    <Map />
+  </div>
+, document.getElementById("map") );
